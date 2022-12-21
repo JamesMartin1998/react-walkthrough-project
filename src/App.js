f9b82be6983d42ba84a294aa94d87d0d8627c0ad
JamesMartin1998 <jamesandrewmartin1998@hotmail.co.uk>
2022-12-21 15:17:19 +0000

    add ability to edit and delete posts

--- src/App.js
+++ src/App.js
@@ -1,63 +1,60 @@
 import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container";
-import {Route, Switch} from "react-router-dom";
-import './api/axiosDefaults';
+import { Route, Switch } from "react-router-dom";
+import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
-import { createContext, useEffect, useState } from "react";
-import axios from "axios";
 import PostCreateForm from "./pages/posts/PostCreateForm";
 import PostPage from "./pages/posts/PostPage";
-
-// For each variable you want to be available in another
-// component, need to make a context variable
-export const CurrentUserContext = createContext();
-export const SetCurrentUserContext = createContext();
+import PostsPage from "./pages/posts/PostsPage";
+import { useCurrentUser } from "./contexts/CurrentUserContext";
 
 function App() {
-
-  const [currentUser, setCurrentUser] = useState(null)
-
-  const handleMount = async () => {
-    try {
-      const {data} = await axios.get('dj-rest-auth/user/')
-      setCurrentUser(data);
-    } catch (err) {
-      console.log(err);
-    }
-  }
-
-  useEffect(() => {
-    handleMount();
-  }, []);
+  const currentUser = useCurrentUser();
+  const profile_id = currentUser?.profile_id || "";
 
   return (
-    // By wrapping all elements in the context providers, we can pass
-    // in values to all of the components
-    <CurrentUserContext.Provider value={currentUser}>
-      <SetCurrentUserContext.Provider value={setCurrentUser}>
-
-        <div className={styles.App}>
-        <NavBar />
-        <Container className={styles.Main}>
-          <Switch>
-            <Route exact path="/" render={() => <h1>Home page</h1>} />
-            <Route exact path="/signin" render={() => <SignInForm />} />
-            <Route exact path="/signup" render={() => <SignUpForm />} />
-            <Route exact path="/posts/create" render={() => <PostCreateForm />} />
-            <Route exact path="/posts/:id" render={() => <PostPage />} />
-            <Route render={() => <h1>Page not found!</h1>} />
-          </Switch>
-        </Container>
-        </div>
-
-      </SetCurrentUserContext.Provider>
-    </CurrentUserContext.Provider>
-
-
-    
-  )
+    <div className={styles.App}>
+      <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <PostsPage message="No results found. Adjust the search keyword." />
+            )}
+          />
+          <Route
+            exact
+            path="/feed"
+            render={() => (
+              <PostsPage
+                message="No results found. Adjust the search keyword or follow a user."
+                filter={`owner__followed__owner__profile=${profile_id}&`}
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/liked"
+            render={() => (
+              <PostsPage
+                message="No results found. Adjust the search keyword or like a post."
+                filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`}
+              />
+            )}
+          />
+          <Route exact path="/signin" render={() => <SignInForm />} />
+          <Route exact path="/signup" render={() => <SignUpForm />} />
+          <Route exact path="/posts/create" render={() => <PostCreateForm />} />
+          <Route exact path="/posts/:id" render={() => <PostPage />} />
+          <Route render={() => <p>Page not found!</p>} />
+        </Switch>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
