7990f781846686f094999d1256e4e6381c46098b
JamesMartin1998 <jamesandrewmartin1998@hotmail.co.uk>
2022-12-31 13:02:46 +0000

    set up tests

---
+++ src/mocks/handlers.js
@@ -0,0 +1,23 @@
+import { rest } from "msw";
+
+const baseURL = "https://d-rest-framework.herokuapp.com/";
+
+export const handlers = [
+  rest.get(`${baseURL}dj-rest-auth/user/`, (req, res, ctx) => {
+    return res(
+      ctx.json({
+        pk: 2,
+        username: "james",
+        email: "",
+        first_name: "",
+        last_name: "",
+        profile_id: 2,
+        profile_image:
+          "https://res.cloudinary.com/dylqwyatz/image/upload/v1/media/../default_profile_f7y9cq",
+      })
+    );
+  }),
+  rest.post(`${baseURL}dj-rest-auth/logout`, (req,res,ctx) => {
+    return res(ctx.status(200))
+  })
+];
--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -5,12 +5,14 @@ import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
 import appStyles from "../../App.module.css";
-import { useParams } from "react-router-dom";
+import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
+import Comment from "../comments/Comment";
+
 import CommentCreateForm from "../comments/CommentCreateForm";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
-import Comment from "../comments/Comment";
+
 import InfiniteScroll from "react-infinite-scroll-component";
 import Asset from "../../components/Asset";
 import { fetchMoreData } from "../../utils/utils";
@@ -18,6 +20,7 @@ import { fetchMoreData } from "../../utils/utils";
 function PostPage() {
   const { id } = useParams();
   const [post, setPost] = useState({ results: [] });
+
   const currentUser = useCurrentUser();
   const profile_image = currentUser?.profile_image;
   const [comments, setComments] = useState({ results: [] });
@@ -25,9 +28,9 @@ function PostPage() {
   useEffect(() => {
     const handleMount = async () => {
       try {
-        const [{ data: post }, {data:comments}] = await Promise.all([
+        const [{ data: post }, { data: comments }] = await Promise.all([
           axiosReq.get(`/posts/${id}`),
-          axiosReq.get(`/comments/?post=${id}`)
+          axiosReq.get(`/comments/?post=${id}`),
         ]);
         setPost({ results: [post] });
         setComments(comments);
@@ -35,6 +38,7 @@ function PostPage() {
         console.log(err);
       }
     };
+
     handleMount();
   }, [id]);
 
@@ -55,22 +59,21 @@ function PostPage() {
           ) : comments.results.length ? (
             "Comments"
           ) : null}
-
           {comments.results.length ? (
             <InfiniteScroll
-            children={comments.results.map((comment) => (
-              <Comment
-                key={comment.id}
-                {...comment}
-                setPost={setPost}
-                setComments={setComments}
-              />
-            ))}
-            dataLength={comments.results.length}
-            loader={<Asset spinner />}
-            hasMore={!!comments.next}
-            next={() => fetchMoreData(comments, setComments)}
-          />
+              children={comments.results.map((comment) => (
+                <Comment
+                  key={comment.id}
+                  {...comment}
+                  setPost={setPost}
+                  setComments={setComments}
+                />
+              ))}
+              dataLength={comments.results.length}
+              loader={<Asset spinner />}
+              hasMore={!!comments.next}
+              next={() => fetchMoreData(comments, setComments)}
+            />
           ) : currentUser ? (
             <span>No comments yet, be the first to comment!</span>
           ) : (
